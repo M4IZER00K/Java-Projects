25568f6b94285685f9a4f5da0613930c10e2f9c6
M4IZER00K <65840917+M4IZER00K@users.noreply.github.com>
2021-05-31 15:54:19 -0700

    Add files via upload

---
+++ programmingProject2/src/programmingProject2/programmingProject2.java
@@ -0,0 +1,33 @@
+package programmingProject2;
+
+public class programmingProject2 {
+	public static void main(String[] args) {
+		Classes();
+		Hobbies();
+		Money();
+	}
+	public static void Base() {
+		System.out.println("Hello, how are you doing?");
+		System.out.println("I hope you are doing well.");
+		System.out.println("I was wondering how are/is your classes/work going for you.");
+		System.out.println("Overall, life has been pretty boring for me except for a few fun activites ive done over summer.");
+	}
+	public static void Classes() {
+		Base();
+		System.out.println("However, my classes have been going pretty well so far.");
+		System.out.println("Some of my classes are a bit challenging, but very entertaining!");
+		System.out.println("Many classes are difficult to learn online especially when the classes are maent to be played together, for example band posses that challenge\n");
+	}
+	public static void Hobbies() {
+		Base();
+		System.out.println("Playing basketball outside was pretty fun even though it was only me playing.");
+		System.out.println("I also enjoyed playing video games with my friends over summer.");
+		System.out.println("Finally, I took some addintional buisness courses which seemed pretty entertaining\n");
+	}
+	public static void Money(){
+		Base();
+		System.out.println("So while I was playing and enjoying summer I started to run few on money, this is the part where you come in!");
+		System.out.println("Since I know, you also dont have that much money either, I'm just asking for a 100 dollars?");
+		System.out.println("Either way I hope we meet soon and if you can venmo me the money :)\n");
+	}
+}
